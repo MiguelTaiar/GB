904020b0520f9eea665451061bf48256fac3a3a1
migueltaiar <migueltaiar@gmail.com>
2021-09-12 13:05:33 -0300

    Limpeza do codigo e adicao de isGlobal para ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ResellersModule } from './resellers/resellers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_URI),
     ResellersModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
